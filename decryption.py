6c754a7695ff9f6a08891faa4789c0ff8ed5c148
Ali Khokhar <theintrovertedpumpkin@gmail.com>
2025-04-27 11:36:27 -0400

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -1,3 +1,15 @@
+import random
+import string
+
+chars = string.punctuation + string.digits + string.ascii_letters + " "
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key  : {key}")
+
 # DECRYPTION
 
 cipher_text = input("Enter a message to decrypt: ")
