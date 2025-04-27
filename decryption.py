2c383fbf3f67c5fcee4de6f6642179704ab0f3e6
Ali Khokhar <theintrovertedpumpkin@gmail.com>
2025-04-27 11:36:14 -0400

    decryption.py

---
+++ decryption.py
@@ -0,0 +1,11 @@
+# DECRYPTION
+
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message:  {plain_text}")
