b87199e017f4715f16577263206cdc1e22774bac
Ali Khokhar <theintrovertedpumpkin@gmail.com>
2025-04-27 11:35:54 -0400

    encryption.py

--- encryption.py
+++ encryption.py
@@ -21,15 +21,3 @@ for letter in plain_text:
 print(f"original message:  {plain_text}")
 print(f"encrypted message: {cipher_text}")
 
-# DECRYPTION
-
-cipher_text = input("Enter a message to decrypt: ")
-plain_text = ""
-
-for letter in cipher_text:
-    index = key.index(letter)
-    plain_text += chars[index]
-
-print(f"encrypted message: {cipher_text}")
-print(f"original message:  {plain_text}")
-
